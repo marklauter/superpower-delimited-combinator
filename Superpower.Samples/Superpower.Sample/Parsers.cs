894765973cfa82ebe2283ceda2b3f76e87d88553
Mark Lauter <mark@marklauter.com>
2024-03-21 19:21:51 -0400

    consecutive semicolons

---
+++ Superpower.Samples/Superpower.Sample/Parsers.cs
@@ -0,0 +1,60 @@
+﻿using Superpower;
+using Superpower.Display;
+using Superpower.Model;
+using Superpower.Parsers;
+using Superpower.Tokenizers;
+using System.Diagnostics.CodeAnalysis;
+
+internal enum DelimtedTextToken
+{
+    [Token(Category = "delimiter", Example = ";")]
+    Semicolon,
+
+    Value,
+
+    NullValue,
+}
+
+internal static class TextParsers
+{
+    public static TextParser<string> NullValue { get; } =
+        from start in Character.EqualTo(';')
+        from ignore in Character.WhiteSpace.Many()
+        from close in Character.EqualTo(';')
+        select "empty";
+}
+
+internal static class Tokenizer
+{
+    public static Tokenizer<DelimtedTextToken> Instance { get; } =
+        new TokenizerBuilder<DelimtedTextToken>()
+            .Ignore(Span.WhiteSpace)
+            .Match(TextParsers.NullValue, DelimtedTextToken.NullValue)
+            .Match(Character.EqualTo(';'), DelimtedTextToken.Semicolon)
+            .Match(Character.Except(';').Many(), DelimtedTextToken.Value)
+            .Build();
+}
+
+internal static class DelimitedListParser
+{
+    public static bool TryParse(
+        string input,
+        out TokenList<DelimtedTextToken>? value,
+        [MaybeNullWhen(true)] out string error,
+        out Position errorPosition)
+    {
+        var tokens = Tokenizer.Instance.TryTokenize(input);
+        if (!tokens.HasValue)
+        {
+            value = null;
+            error = tokens.ToString();
+            errorPosition = Position.Empty;
+            return false;
+        }
+
+        value = tokens.Value;
+        error = null;
+        errorPosition = Position.Empty;
+        return true;
+    }
+}
---
+++ Superpower.Samples/Superpower.Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿var inputs = new string[]
+{
+    "TEST123;TEST456;;HALLO",
+    "TEST123;TEST456; ;HALLO",
+    "TEST123;TEST456;  ;HALLO",
+};
+
+for (var i = 0; i < inputs.Length; ++i)
+{
+    var input = inputs[i];
+
+    var parsed = DelimitedListParser.TryParse(
+        input,
+        out var value,
+        out var error,
+        out var errorPosition);
+
+    var output = parsed && value.HasValue
+        ? String.Join(Environment.NewLine, value.Value)
+        : $"err: {error}, pos: {errorPosition}";
+
+    Console.WriteLine($"INPUT: {input}");
+    Console.WriteLine("OUTPUT:");
+    Console.WriteLine(output);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
